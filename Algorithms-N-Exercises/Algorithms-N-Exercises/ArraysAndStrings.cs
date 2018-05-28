7f55cdddb648901219c44dd8112d6270118c459c
bin0m <levitg@gmail.com>
2018-05-28 19:41:38 +0300

    IsStringUnique

---
+++ Algorithms-N-Exercises/Algorithms-N-Exercises/ArraysAndStrings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_N_Exercises
+{
+    public static class ArraysAndStrings
+    {
+        // IsStringUnique2
+        // O(n)
+        public static bool IsStringUnique(String str)
+        {
+            //Edge conditions
+            if (string.IsNullOrEmpty(str))
+            {
+                return true;
+            }
+            int[] mem = new int[256];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                mem[(int)str[i]]++;
+                if (mem[str[i]] > 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // IsStringUnique2 without using extra space
+        // O(nlogn)
+        public static bool IsStringUnique2(String str)
+        {
+            //Edge conditions
+            if (string.IsNullOrEmpty(str))
+            {
+                return true;
+            }
+            var chars = str.ToArray();
+            Array.Sort(chars);
+            for (int i = 1; i < chars.Length; i++)
+            {
+                if (chars[i] == chars[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Algorithms-N-Exercises/Algorithms-N-Exercises/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_N_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"IsUnique: result={ArraysAndStrings.IsStringUnique("dFf")}, expected=True");
+            Console.WriteLine($"IsUnique: result={ArraysAndStrings.IsStringUnique("dFfd")}, expected=False");
+            Console.WriteLine($"IsUnique2: result={ArraysAndStrings.IsStringUnique2("dFf")}, expected=True");
+            Console.WriteLine($"IsUnique2: result={ArraysAndStrings.IsStringUnique2("dFfd")}, expected=False");
+            Console.ReadLine();
+        }
+    }
+}
