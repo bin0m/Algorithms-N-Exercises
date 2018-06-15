e637f2ef051d096bc4af27fe6438255e66cd73fa
bin0m <levitg@gmail.com>
2018-06-15 22:06:44 +0300

    LinkedLists NthToLast()

---
+++ Algorithms-N-Exercises/Algorithms-N-Exercises/LinkedLists.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_N_Exercises
+{
+    public class LinkedLists
+    {
+        public class ListNode<T>
+        {
+            public ListNode<T> next;
+            public T val;
+            public ListNode(T val)
+            {
+                this.val = val;
+            }
+        }
+
+        static int currentNthToLast = 0;
+
+        public ListNode<int> NthToLast(ListNode<int> head, int n)
+        {
+            if(head == null)
+            {
+                return null;
+            }
+
+            ListNode<int> node = NthToLast(head.next, n);
+
+            if (currentNthToLast == n)
+            {
+                return head;
+            }
+
+            currentNthToLast++;            
+
+            return node;
+
+        }
+
+
+    }
+}
