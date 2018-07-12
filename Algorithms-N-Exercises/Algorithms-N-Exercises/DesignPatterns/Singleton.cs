64f281bb071c3b225db510e2679d1b01c900c27f
Gregory Levit <levitg@gmail.com>
2018-07-13 02:21:14 +0300

    Singleton (Design Pattern) & tests

---
+++ Algorithms-N-Exercises/Algorithms-N-Exercises/DesignPatterns/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Algorithms_N_Exercises.DesignPatterns
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string city);
+    }
+    
+    public class SafeSingletonDatabase : IDatabase
+    {
+        private static Lazy<SafeSingletonDatabase> _instance =
+            new Lazy<SafeSingletonDatabase>(() => new SafeSingletonDatabase());
+
+        public static SafeSingletonDatabase Instance => _instance.Value;
+
+        private static int _countOfInstances;
+        public static int CountOfInstances => _countOfInstances;
+
+        private readonly Dictionary<string, int> _capitals;
+
+        private SafeSingletonDatabase()
+        {
+            _countOfInstances++;
+            Console.WriteLine("Initializing SafeSingletonDatabase");
+            _capitals = File.ReadAllLines("capitals.txt")
+                .ToDictionary(line => line.Trim().Split(';')[0], line => int.Parse(line.Trim().Split(';')[1]));
+
+        }
+
+        public int GetPopulation(string city)
+        {
+            return _capitals[city];
+        }
+    }
+}
---
+++ Algorithms-N-Exercises/Algorithms_N_Exercises.UnitTests/DesignPatternsTests/SingletonTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Algorithms_N_Exercises.DesignPatterns;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms_N_Exercises.UnitTests.DesignPatternsTests
+{
+    [TestClass]
+    public class SingletonTests
+    {
+        [TestMethod]
+        public void IsSingletonTest()
+        {
+            var instance1 = SafeSingletonDatabase.Instance;
+            var instance2 = SafeSingletonDatabase.Instance;
+            Assert.AreSame(instance1,instance2);
+            Assert.AreEqual(SafeSingletonDatabase.CountOfInstances,1);
+        }
+
+        [TestMethod]
+        public void IsCapitalsDBWorks()
+        {
+            var db = SafeSingletonDatabase.Instance;
+            Assert.AreEqual(db.GetPopulation("Osaka"), 16425000);
+        }
+    }
+}
