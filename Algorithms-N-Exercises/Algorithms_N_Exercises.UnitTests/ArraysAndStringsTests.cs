95e01d8e61fda166ec1395ab604f1b5bd6d5e99b
bin0m <levitg@gmail.com>
2018-07-13 19:24:26 +0300

    MeetingPlanner solution with unit tests

--- Algorithms-N-Exercises/Algorithms_N_Exercises.UnitTests/ArraysAndStringsTests.cs
+++ Algorithms-N-Exercises/Algorithms_N_Exercises.UnitTests/ArraysAndStringsTests.cs
@@ -94,5 +94,50 @@ namespace Algorithms_N_Exercises.UnitTests
         {
             Assert.IsTrue(ArraysAndStrings.CheckPermutation("abcd", "dbac"));
         }
+
+        [TestMethod]
+        public void MeetingPlannerTest1()
+        {
+            var slotA = new[,] { { 7, 12 } };
+            var slotB = new[,] { { 2, 11 } };
+            int dur = 5;
+            Assert.IsNull(ArraysAndStrings.MeetingPlanner(slotA, slotB, dur));
+        }
+
+        [TestMethod]
+        public void MeetingPlannerTest2()
+        {
+            var slotA = new[,] { { 6, 12 } };
+            var slotB = new[,] { { 2, 11 } };
+            int dur = 5;
+            CollectionAssert.AreEqual(ArraysAndStrings.MeetingPlanner(slotA, slotB, dur), new[] { 6, 11 });
+        }
+
+        [TestMethod]
+        public void MeetingPlannerTest3()
+        {
+            var slotA = new[,] { { 1, 10 } };
+            var slotB = new[,] { { 2, 3 }, { 5, 7 } };
+            int dur = 2;
+            CollectionAssert.AreEqual(ArraysAndStrings.MeetingPlanner(slotA, slotB, dur), new[] { 5, 7 });
+        }
+
+        [TestMethod]
+        public void MeetingPlannerTest4()
+        {
+            var slotA = new[,] { { 0, 5 }, { 50, 70 }, { 120, 125 } };
+            var slotB = new[,] { { 0, 50 } };
+            int dur = 8;
+            Assert.IsNull(ArraysAndStrings.MeetingPlanner(slotA, slotB, dur));
+        }
+
+        [TestMethod]
+        public void MeetingPlannerTest5()
+        {
+            var slotA = new[,] { { 10, 50 }, { 60, 120 }, { 140, 210 } };
+            var slotB = new[,] { { 0, 15 }, { 60, 70 } };
+            int dur = 8;
+            CollectionAssert.AreEqual(ArraysAndStrings.MeetingPlanner(slotA, slotB, dur), new[] { 60, 68 });
+        }
     }
 }
