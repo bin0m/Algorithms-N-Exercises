dd670c23f381318b343bc3f7bacf30f6deb69f80
bin0m <levitg@gmail.com>
2018-09-10 20:15:01 +0300

    Add Tree tests

--- Algorithms-N-Exercises/Algorithms-N-Exercises/Trees.cs
+++ Algorithms-N-Exercises/Algorithms-N-Exercises/Trees.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.CompilerServices;
 
 namespace Algorithms_N_Exercises
-{
+{   
     public class Trees
     {
         public class TreeNode
---
+++ Algorithms-N-Exercises/Algorithms_N_Exercises.UnitTests/TressTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_N_Exercises.UnitTests
+{
+    [TestClass]
+    public class TressTests
+    {
+        [TestMethod]
+        public void ListOfDepthsTest1()
+        {
+            var res01 = Trees.ListOfDepths(Trees.CreateBinaryTree(new int?[] { 3, 9, 20, null, null, 15, 7 }));
+            Console.WriteLine($"result=[[{res01[0].First.Value.Val}],[{res01[1].First.Value.Val},{res01[1].Last.Value.Val}],[{res01[2].First.Value.Val},{res01[2].Last.Value.Val}]],expected = [[3],[9,20],[15,7]]");
+            //Assert.AreEqual()
+
+        }
+    }
+}
