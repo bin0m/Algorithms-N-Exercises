3a086c4dfce4e7051c54857cb0acc8e4a1cef18b
bin0m <levitg@gmail.com>
2018-09-11 18:34:19 +0300

    fix test

--- Algorithms-N-Exercises/Algorithms_N_Exercises.UnitTests/TressTests.cs
+++ Algorithms-N-Exercises/Algorithms_N_Exercises.UnitTests/TressTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Algorithms_N_Exercises.Trees;
 
 namespace Algorithms_N_Exercises.UnitTests
 {
@@ -13,9 +14,14 @@ namespace Algorithms_N_Exercises.UnitTests
         [TestMethod]
         public void ListOfDepthsTest1()
         {
-            var res01 = Trees.ListOfDepths(Trees.CreateBinaryTree(new int?[] { 3, 9, 20, null, null, 15, 7 }));
-            Console.WriteLine($"result=[[{res01[0].First.Value.Val}],[{res01[1].First.Value.Val},{res01[1].Last.Value.Val}],[{res01[2].First.Value.Val},{res01[2].Last.Value.Val}]],expected = [[3],[9,20],[15,7]]");
-            //Assert.AreEqual()
+            var res01 = ListOfDepths(CreateBinaryTree(new int?[] { 3, 9, 20, null, null, 15, 7 }));
+
+            Assert.AreEqual(3, res01.Count());
+            Assert.AreEqual(3, res01[0]?.First?.Value?.Val);
+            Assert.AreEqual(9, res01[1]?.First?.Value?.Val);
+            Assert.AreEqual(20, res01[1]?.Last?.Value?.Val);
+            Assert.AreEqual(15, res01[2]?.First?.Value?.Val);
+            Assert.AreEqual(7, res01[2]?.Last?.Value?.Val);
 
         }
     }
