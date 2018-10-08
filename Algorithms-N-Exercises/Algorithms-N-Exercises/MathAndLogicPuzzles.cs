14bb9ccf3c11d09f66488b18b69aa305858d2ede
bin0m <levitg@gmail.com>
2018-10-08 21:00:55 +0300

    digPow for codewars

--- Algorithms-N-Exercises/Algorithms-N-Exercises/MathAndLogicPuzzles.cs
+++ Algorithms-N-Exercises/Algorithms-N-Exercises/MathAndLogicPuzzles.cs
@@ -36,5 +36,30 @@ namespace Algorithms_N_Exercises
 
             return Math.Round(mid, 2);
         }
+
+        // Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p 
+        // we want to find a positive integer k, if it exists, such as the sum 
+        // of the digits of n taken to the successive powers of p is equal to k * n. 
+        // 89 --> 8¹ + 9² = 89 * 1
+        // 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+        // 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+        // If it is the case we will return k, if not return -1.
+        public static long digPow(int n, int p)
+        {
+            int countOfDigitsMinusOne = (int)Math.Log10(n);
+            p = p + countOfDigitsMinusOne;
+            int nk = 0;
+            int N = n;
+            while (N != 0)
+            {
+                int digit = N % 10;
+                nk += (int)Math.Pow(digit, p);
+                N /= 10;
+                p--;
+            }
+            if (nk % n == 0)
+                return nk / n;
+            return -1;
+        }
     }
 }
